fe0ef5e707b46bf31e8cada7e8c6ee633f171e3c
Postmodern <postmodern.mod3@gmail.com>
2020-12-31 04:50:24 -0800

    Fixed aliases.

--- lib/cve_schema/cve/impact.rb
+++ lib/cve_schema/cve/impact.rb
@@ -8,12 +8,12 @@ module CVESchema
       # @return [CVSSv2, nil]
       attr_reader :cvssv2
 
-      alias cvssv2 cvss_v2
+      alias cvss_v2 cvssv2
 
       # @return [CVSSv3, nil]
       attr_reader :cvssv3
 
-      alias cvssv3 cvss_v3
+      alias cvss_v3 cvssv3
 
       def initialize(cvssv2: nil, cvssv3: nil)
         @cvssv2 = cvssv2
