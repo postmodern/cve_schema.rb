c8d0b6f10de46635ece2b34476f0f7408288022e
Postmodern <postmodern.mod3@gmail.com>
2021-01-05 04:13:01 -0800

    dded specs for ProblemType.

---
+++ spec/problem_type_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require 'shared_examples'
+require 'cve_schema/cve/problem_type'
+
+describe CVESchema::CVE::ProblemType do
+  describe "#initialize" do
+    let(:description) { double(:description) }
+
+    subject { described_class.new(description) }
+
+    it "must set #description" do
+      expect(subject.description).to be(description)
+    end
+  end
+
+  describe ".from_json" do
+    include_examples ".from_json"
+
+    let(:json_node) { json_tree['problemtype']['problemtype_data'][0] }
+
+    context '"description":' do
+      it { expect(subject.description).to_not be_empty }
+      it { expect(subject.description).to all(be_kind_of(CVESchema::CVE::Description)) }
+    end
+  end
+end
