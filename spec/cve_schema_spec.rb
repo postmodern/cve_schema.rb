d11bb27ecd5442f78987f0d1b257c50405273912
Postmodern <postmodern.mod3@gmail.com>
2021-01-05 05:31:55 -0800

    Fixed a spec.

--- spec/cve_schema_spec.rb
+++ spec/cve_schema_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'cve_schema'
 
-describe CveSchema do
+describe CVESchema do
   it "should have a VERSION constant" do
     expect(subject.const_get('VERSION')).to_not be_empty
   end
