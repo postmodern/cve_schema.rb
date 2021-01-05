fc934b680ceabb0c9cb7e35dd7431b0af2398081
Postmodern <postmodern.mod3@gmail.com>
2021-01-05 04:54:12 -0800

    Added specs for Timeline.

---
+++ spec/timeline_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+require 'shared_examples'
+require 'cve_schema/cve/timeline'
+
+describe CVESchema::CVE::Timeline do
+  it { expect(described_class).to include(CVESchema::CVE::HasLangValue) }
+
+  describe "#initialize" do
+    let(:time)  { Time.now }
+    let(:lang)  { 'eng' }
+    let(:value) { 'Initial publication' }
+
+    describe "required keywords" do
+      context "when time: is not given" do
+        it do
+          expect {
+            described_class.new(lang: lang, value: value)
+          }.to raise_error(ArgumentError)
+        end
+      end
+
+      context "when lang: is not given" do
+        it do
+          expect {
+            described_class.new(time: time, value: value)
+          }.to raise_error(ArgumentError)
+        end
+      end
+
+      context "when value: is not given" do
+        it do
+          expect {
+            described_class.new(time: time, lang: lang)
+          }.to raise_error(ArgumentError)
+        end
+      end
+    end
+
+    subject do
+      described_class.new(
+        time: time,
+        lang: lang,
+        value: value
+      )
+    end
+
+    it "must set #time" do
+      expect(subject.time).to eq(time)
+    end
+
+    it "must set #lang" do
+      expect(subject.lang).to eq(lang)
+    end
+
+    it "must set #value" do
+      expect(subject.value).to eq(value)
+    end
+  end
+
+  describe ".from_json" do
+    include_examples ".from_json"
+
+    let(:json_node) { json_tree['timeline'][0] }
+
+    context '"time":' do
+      it { expect(subject.time).to be == DateTime.parse(json_node['time']) }
+    end
+
+    context '"lang":' do
+      it { expect(subject.lang).to be == json_node['lang'] }
+    end
+
+    context '"value":' do
+      it { expect(subject.value).to be == json_node['value'] }
+    end
+  end
+end
